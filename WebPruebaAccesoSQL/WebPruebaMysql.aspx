381465025c9f1e54fb424b44a9faf9a813e8fe6d
Luis Enrique <utp0008625@alumno.utpuebla.edu.mx>
2021-07-29 13:29:34 -0500

    07 usaMysql, ya se conecta a servidor MySQL, y hace una consulta a la tabla materia con un gridView

--- WebPruebaAccesoSQL/WebPruebaMysql.aspx
+++ WebPruebaAccesoSQL/WebPruebaMysql.aspx
@@ -15,6 +15,30 @@
             <br />
             <h2>Base de datos mysql</h2>
         </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Probar conexión con Mysql" Width="328px" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Password:"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox2" runat="server" Width="849px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Consultas de materias" />
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3">
+            <FooterStyle BackColor="White" ForeColor="#000066" />
+            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+            <RowStyle ForeColor="#000066" />
+            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#00547E" />
+        </asp:GridView>
     </form>
 </body>
 </html>
--- WebPruebaAccesoSQL/WebPruebaMysql.aspx.cs
+++ WebPruebaAccesoSQL/WebPruebaMysql.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace WebPruebaAccesoSQL
 {
@@ -11,6 +13,80 @@ namespace WebPruebaAccesoSQL
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private MySqlConnection conexionMysql(ref string mensaje)
+        {
+            //crear la conexión a la base de datos
+            MySqlConnection conecMysql = new MySqlConnection();
+            conecMysql.ConnectionString = "Server = localhost; Port=3306; Database = escuela; Uid = root; Pwd =" +
+                TextBox1.Text + ";";
+            try
+            {
+                conecMysql.Open();
+                mensaje = "Conexión abierta con Mysql";
+            }
+            catch (Exception h)
+            {
+                mensaje = "Error: " + h.Message;
+                conecMysql = null;
+            }
+
+            return conecMysql;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //abrir la conexion
+
+            //solo referencias
+            MySqlConnection conecNueva = null;
+            MySqlCommand carrito = null;
+            DataSet contenedor = null;
+            MySqlDataAdapter trailer = null;
+
+            string query = "select * from materia";
+            string m = "";
+            conecNueva = conexionMysql(ref m);
+
+            if(conecNueva != null)
+            {
+                //creo la consulta, y le indico por cual corretera ira.
+                carrito = new MySqlCommand();
+                carrito.CommandText = query;
+                carrito.Connection = conecNueva;
+
+                trailer = new MySqlDataAdapter();
+                trailer.SelectCommand = carrito;
+
+                //llenar el dataSet
+                contenedor = new DataSet();
+                try
+                {
+                    trailer.Fill(contenedor);
+                    TextBox2.Text = "Consulta correcta";
+                    GridView1.DataSource = contenedor.Tables[0];
+                    GridView1.DataBind();
+                }
+                catch(Exception w)
+                {
+                    TextBox2.Text = "Erro: " + w.Message;
+                }
+                conecNueva.Close();
+                conecNueva.Dispose();
+        
+            }
+            else
+            {
+                TextBox2.Text = m;
+            }
 
         }
     }
